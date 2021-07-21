ae4d1bed57fb2817b39978cecd49ccfc8030f85f
Empty-Engineering <34143609+Empty-Engineering@users.noreply.github.com>
2021-07-21 22:22:36 +0200

    Update Physics.cpp

--- Physics.cpp
+++ Physics.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
-#include <math.h>
-
-
-class Physics{
+#include <tgmath.h>
+class Kinematics{
 	private:
 		float time, displacement, velocity, acceleration;
     public:
-		Physics(float time, float displacement, float velocity, float acceleration){
+		Kinematics(float time, float displacement, float velocity, float acceleration){
 			time = time;
 			displacement = displacement;
 			velocity = velocity;
 			acceleration = acceleration;
 		}
-	float get_Time(){
+	float get_time(){
 		if (acceleration != 0 && displacement != 0){
 			return sqrt(((2*displacement)/acceleration));
 		}else if (velocity != 0 && displacement != 0){
@@ -22,6 +19,29 @@ class Physics{
 		}
 	}
 	float get_displacement(){
-		
+		if (acceleration != 0 || velocity !=0){
+			return (velocity*time+(0.5*(acceleration*time*time)));
+		}else{
+			return displacement;
+		}
+	}
+	float get_velocity(){
+		if (time != 0){
+			return (displacement/time)+(acceleration*time);
+		}else{
+			return velocity;
+		}
+	}
+	float get_acceleration(){
+		if (velocity != 0 && time != 0){
+			return velocity/time;
+		}else if (displacement != 0 && time != 0){
+			return displacement/(time*time);
+		}else{
+			return acceleration;
+		}
 	}
 };
+int main(){
+	return 0;
+}
