0dbccbff70758fadc62c697e4c2077c7d26c2568
Lukas Robin <lukasmrobin@gmail.com>
2021-08-30 17:23:43 -0400

    Added Bug fixes and exception handling

--- Source/Physics.cpp
+++ Source/Physics.cpp
@@ -3,33 +3,34 @@
 #include <stdio.h>
 #include "Dependency/kinematiclib.h"
 using namespace std;
-	float get_time(float time, float displacement, float velocity, float acceleration){
-		if (time != 0){
-			return time;
-		}else if (acceleration != 0 && displacement != 0){
-			return sqrt(((2*displacement)/acceleration));
-		}else if (velocity != 0 && displacement != 0){
-			return displacement/velocity;
-		}else{
-			throw logic_error;
-		}
+int Value_Error = 0;
+float get_time(float time, float displacement, float velocity, float acceleration){
+	if (time != 0){
+		return time;
+	}else if (acceleration != 0 && displacement != 0){
+		return sqrt(((2*displacement)/acceleration));
+	}else if (velocity != 0 && displacement != 0){
+		return displacement/velocity;
+	}else{
+		throw Value_Error;
 	}
-	float get_displacement(float time, float displacement, float velocity, float acceleration){
-		if (displacement != 0){
-			return displacement;
-		}else if (acceleration != 0 || velocity !=0){
-			return (velocity*time+(0.5*(acceleration*time*time)));
-		}else{
-			throw logic_error;
-		}
+}
+float get_displacement(float time, float displacement, float velocity, float acceleration){
+	if (displacement != 0){
+		return displacement;
+	}else if (acceleration != 0 || velocity !=0){
+		return (velocity*time+(0.5*(acceleration*time*time)));
+	}else{
+		throw Value_Error;
 	}
-	float get_velocity(float time, float displacement, float velocity, float acceleration){
-		if (velocity != 0){
-			return velocity;
-		}else if (time != 0){
+}
+float get_velocity(float time, float displacement, float velocity, float acceleration){
+	if (velocity != 0){
+		return velocity;
+	}else if (time != 0){
 			return ((displacement/time)+(acceleration * time));
 		}else{
-			throw logic_error;
+			throw Value_Error;
 		}
 	}
 	float get_acceleration(float time, float displacement, float velocity, float acceleration){
@@ -41,7 +42,7 @@ using namespace std;
 			return (displacement/(time * time));
 		}
 		else{
-			throw logic_error;
+			throw Value_Error;
 		}
 	}
 int main(){
@@ -63,17 +64,32 @@ int main(){
 	std::cin >> wtfDoYouWant;
 	if (wtfDoYouWant == "time"){
 		float time = get_time(*ptrTime, *ptrDisp, *ptrVel, *ptrAcc);
-		//Update to include catch exception then exit
+		try{
 		printf("t = %f\n", time);
+		}catch (int Value_Error){
+			std::cout << "Value error, please try again. \n";
+		}
 	}else if (wtfDoYouWant == "displacement"){
 		float displacement = get_displacement(*ptrTime, *ptrDisp, *ptrVel, *ptrAcc);
+		try{
 		printf("d = %f\n", displacement);
+		}catch (int Value_Error){
+			std::cout << "Value error, please try again. \n";
+		}
 	}else if (wtfDoYouWant == "velocity"){
 		float velocity = get_velocity(*ptrTime, *ptrDisp, *ptrVel, *ptrAcc);
+		try{
 		printf("v = %f\n", velocity);
+		}catch (int Value_Error){
+			std::cout << "Value error, please try again. \n";
+		}
 	}else if (wtfDoYouWant == "acceleration"){
 		float acceleration = get_acceleration(*ptrTime, *ptrDisp, *ptrVel, *ptrAcc);
+		try{
 		printf("a = %f\n", acceleration);
+		}catch (int Value_Error){
+			std::cout << "Value error, please try again. \n";
+		}
 	}
 	printf("-----------------------------------------------------------------\n");
 };
